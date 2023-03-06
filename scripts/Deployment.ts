83a82cffdcdfd82fe44be090d703b73c17193373
Noam <noamru22@gmail.com>
2023-03-06 12:40:47 +0100

    added comments

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -12,34 +12,46 @@ const convertStringArrayToBytes32 = (array: string[]) => {
 };
 
 async function main() {
+  // pick the arguments
   const args = process.argv;
   const proposals = args.slice(2);
-
   if (proposals.length <= 0) throw new Error("Missing parameters proposals");
-  //   what is a provider? abstraction of a connection to the ETH network
+
+  // setup the provider
+  //   provide is the connection to the blockchain
   const provider = ethers.getDefaultProvider("goerli");
   //   const provider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY);
 
+  // setup wallet
   const mnemonic = process.env.MNEMONIC;
   if (!mnemonic || mnemonic.length <= 0)
     throw new Error("Missing environment: Mnemonic seed");
-
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
   console.log(`Connected to the wallet address ${wallet.address}`);
+
+  // connecting signer
   const signer = wallet.connect(provider);
+  // check balance
   const balance = await signer.getBalance();
   console.log(`Wallet balance: ${balance} Wei`);
 
+  //console.log output
   console.log("Deploying Ballot contract");
   console.log("Proposals: ");
   proposals.forEach((element, index) => {
     console.log(`Proposal N. ${index + 1}: ${element}`);
   });
+
+  // make the ballotFactory
   const ballotContractFactory = new Ballot__factory(signer);
+
+  // deploy contract
   const ballotContract = await ballotContractFactory.deploy(
     convertStringArrayToBytes32(proposals)
   );
   const deployTxReceipt = await ballotContract.deployTransaction.wait();
+
+  // console.log output
   console.log(
     `The Ballot contract was deployed at the address ${ballotContract.address}`
   );
